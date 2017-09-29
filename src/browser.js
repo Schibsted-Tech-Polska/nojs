7e1cd0cc17eb846014e4e499173b822de5599a38
Wojciech Iskra <wojciech.iskra@schibsted.com>
2017-09-29 09:38:04 +0200

    Restoring long living Chromium process, setting pages limit to 100 to be safe.
    Works fine with proper `--shm-size` setting in `docker run` command.

--- src/browser.js
+++ src/browser.js
@@ -4,9 +4,26 @@ const config = require('ez-config').values;
 const puppeteer = require('puppeteer');
 const logger = require('app/logger');
 
-let browser;
+let browser = null;
+let openedUrlsCounter = 0;
+
+const init = async () => {
+    if (browser !== null && openedUrlsCounter < config.puppeteer.maxUrlsOpened) {
+        return;
+    }
+
+    if (openedUrlsCounter >= config.puppeteer.maxUrlsOpened) {
+        logger.debug(`Limit of ${config.puppeteer.maxUrlsOpened} URLs opened reached - restarting Chromium...`);
+        await browser.close();
+        browser = null;
+        openedUrlsCounter = 0;
+    }
+
+    browser = await puppeteer.launch(config.puppeteer.chromeOptions);
+};
 
 const goTo = async (
+    page,
     url,
     options = {
         waitUntil: 'networkidle',
@@ -14,9 +31,10 @@ const goTo = async (
         networkIdleTimeout: 3000,
     }
 ) => {
+    openedUrlsCounter++;
     url = decodeURIComponent(url);
-    logger.debug(`Opening URL: ${url}`);
-    const page = await browser.newPage();
+
+    logger.debug(`Opening URL (${openedUrlsCounter}/${config.puppeteer.maxUrlsOpened}): ${url}`);
 
     if (options.width && options.height) {
         page.setViewport({ width: parseInt(options.width, 10), height: parseInt(options.height, 10) });
@@ -38,21 +56,21 @@ const goTo = async (
 };
 
 const render = async (url, options) => {
-    browser = await puppeteer.launch(config.puppeteer.chromeOptions);
-    const page = await goTo(url, options);
+    await init();
+    const page = await browser.newPage();
+    await goTo(page, url, options);
     const result = await page.content();
     await page.close();
-    await browser.close();
 
     return result;
 };
 
 const screenshot = async (url, options) => {
-    browser = await puppeteer.launch(config.puppeteer.chromeOptions);
-    const page = await goTo(url, options);
+    await init();
+    const page = await browser.newPage();
+    await goTo(page, url, options);
     const result = await page.screenshot({ fullPage: true });
     await page.close();
-    await browser.close();
 
     return result;
 };
