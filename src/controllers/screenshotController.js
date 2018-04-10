79b01a40d1a05868753b112dcdea5ee8cdc0eb6d
Przemysław Babiarz <przemyslaw.babiarz@schibsted.pl>
2018-04-10 10:36:37 +0200

    Added 3 retries for screenshot taking

--- src/controllers/screenshotController.js
+++ src/controllers/screenshotController.js
@@ -3,6 +3,8 @@
 const browser = require('app/browser');
 const encodeData = require('app/utils/encodeData');
 
+const MAX_RETRY_COUNT = 3;
+
 const simpleScreenshot = async (request, reply) => {
     let { url } = request.params;
 
@@ -10,7 +12,7 @@ const simpleScreenshot = async (request, reply) => {
         url += '?' + encodeData(request.query);
     }
 
-    const image = await browser.screenshot(url);
+    const image = await getScreenShot(url);
 
     return reply(image).header('Content-Type', 'image/png');
 };
@@ -18,9 +20,26 @@ const simpleScreenshot = async (request, reply) => {
 const advancedScreenshot = async (request, reply) => {
     let { url, options } = request.payload;
 
-    const image = await browser.screenshot(url, options);
+    const image = await getScreenShot(url, options);
 
     return reply(image).header('Content-Type', 'image/png');
 };
+
+async function getScreenShot(url, options) {
+    let image;
+    let err;
+    for (let i = 0; !image && i < MAX_RETRY_COUNT; i++) {
+        try {
+            image = await browser.screenshot(url, options);
+        } catch (e) {
+            err = e;
+            console.error(e); // eslint-disable-line no-console
+        }
+    }
+    if (!image) {
+        throw err;
+    }
+    return image;
+}
 
 module.exports = { simpleScreenshot, advancedScreenshot };
